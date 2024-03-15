8c92e8d2ed65a8623b5ba7e14ee61a62dec108a6
Codrina Lisaru <94629883+codrinalisaru@users.noreply.github.com>
2024-03-15 08:36:40 +0200

    Create Matrix operations.m

---
+++ Matrix operations.m
@@ -0,0 +1,13 @@
+A = [1 2; 1 -1];
+B = [4 -1; 4 -2];
+% Operatii ce respecta regulile algebrei liniare
+disp('A + B = '); disp(A + B)
+disp('A - B = '); disp(A - B)
+disp('A * B = '); disp(A * B)
+disp('A / B = '); disp(A / B)
+disp('A \ B = '); disp(A \ B)
+disp('A^2 = '); disp(A ^ 2)
+% Operatii element cu element
+disp('A .* B = '); disp(A .* B)
+disp('A ./ B = '); disp(A ./ B)
+disp('A .\ B = '); disp(A .\ B)
