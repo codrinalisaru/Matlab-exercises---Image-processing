ca510f6b580990082668f4b0ef78261780d3c571
Codrina Lisaru <94629883+codrinalisaru@users.noreply.github.com>
2024-03-20 08:26:10 +0200

    Update Define 2D matrix.m

--- Define 2D matrix.m
+++ Define 2D matrix.m
@@ -1,4 +1,4 @@
-% definirea unei matrice cu 2 linii, 3 coloane si 3 straturi
+%define a matrix with 2 lines, 3 columns and 3 layers
 >> A(:,:,1) = [1 4 0; 2 -1 3];
 >> A(:,:,2) = [2 0 -3; 1 0 0];
 >> A(:,:,3) = [1 2 0; 0 1 0];
